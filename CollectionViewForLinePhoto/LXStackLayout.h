f1dd3039e6079904942dc2c9b1733b3217401ddd
leixiang <541150547@qq.com>
2016-12-16 22:15:20 +0800

    提交修改

---
+++ CollectionViewForLinePhoto/LXStackLayout.h
@@ -0,0 +1,13 @@
+//
+//  LXStackLayout.h
+//  CollectionViewForLinePhoto
+//
+//  Created by mac on 16/12/12.
+//  Copyright © 2016年 leixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LXStackLayout : UICollectionViewLayout
+
+@end
---
+++ CollectionViewForLinePhoto/LXStackLayout.m
@@ -0,0 +1,28 @@
+//
+//  LXStackLayout.m
+//  CollectionViewForLinePhoto
+//
+//  Created by mac on 16/12/12.
+//  Copyright © 2016年 leixiang. All rights reserved.
+//
+
+#import "LXStackLayout.h"
+
+@implementation LXStackLayout
+
+- (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds {
+    return YES;
+}
+
+- (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    
+}
+
+-(NSArray<UICollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(CGRect)rect {
+    
+    
+    return <#expression#>
+}
+
+@end
